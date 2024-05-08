e142f028c9fab815b97577ef62805b40654861be
StefanoYepezZ <ayepezz1@upao.edu.pe>
2024-05-07 23:00:35 -0500

    features/crud-contracts

--- src/main/java/com/upao/renteasegrupo1/backingservice/model/dto/ContractRequestDTO.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/model/dto/ContractRequestDTO.java
@@ -1,4 +1,20 @@
 package com.upao.renteasegrupo1.backingservice.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContractRequestDTO {
+    @NotNull(message = "La fecha de inicio no puede ser nula")
+    private LocalDate startDate;
+
+    @NotNull(message = "La fecha de término no puede ser nula")
+    private LocalDate endDate;
+
+    private String status = "Pendientte";//Pendiente es un estado predeterminado
 }
