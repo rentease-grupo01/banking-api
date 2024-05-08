bd65602a3cdd1f730ad30d735e13dce42f23c241
shum3l <ayenglem1@upao.edu.pe>
2024-05-07 23:52:38 -0500

    feature/crud-Lodgin

--- src/main/java/com/upao/renteasegrupo1/backingservice/mapper/ReviewMapper.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/mapper/ReviewMapper.java
@@ -1,4 +1,27 @@
 package com.upao.renteasegrupo1.backingservice.mapper;
+import com.upao.renteasegrupo1.backingservice.model.dto.*;
+import com.upao.renteasegrupo1.backingservice.model.entity.Lodging;
+import com.upao.renteasegrupo1.backingservice.model.entity.Payment;
+import com.upao.renteasegrupo1.backingservice.model.entity.Review;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import java.util.List;
 
+@Component
+@AllArgsConstructor
 public class ReviewMapper {
+    private final ModelMapper modelMapper;
+    public Review convertToEntity(ReviewRequestDTO reviewRequestDTO){
+        return modelMapper.map(reviewRequestDTO, Review.class);
+    }
+
+    public ReviewResponseDTO convertToDTO(Review review) {
+        return modelMapper.map(review, ReviewResponseDTO.class);
+    }
+    public List<ReviewResponseDTO> convertToDTO(List<Review> reviews){
+        return reviews.stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
 }
--- src/main/java/com/upao/renteasegrupo1/backingservice/model/entity/Lodging.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/model/entity/Lodging.java
@@ -1,4 +1,34 @@
 package com.upao.renteasegrupo1.backingservice.model.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name="Lodgings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Lodging {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "capacity", nullable = false)
+    private int capacity;
+    @Column(name = "location", nullable = false)
+    private String location;
+
+
 }
