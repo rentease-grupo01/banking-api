e08162f2eca25a5f78ade226af08d79e25ab675a
StefanoYepezZ <ayepezz1@upao.edu.pe>
2024-05-08 16:53:41 -0500

    features/creacion_de_contratos v2

--- src/main/java/com/upao/renteasegrupo1/backingservice/controller/ContractController.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/controller/ContractController.java
@@ -1,4 +1,40 @@
 package com.upao.renteasegrupo1.backingservice.controller;
 
+import com.upao.renteasegrupo1.backingservice.model.dto.ContractRequestDTO;
+import com.upao.renteasegrupo1.backingservice.model.dto.ContractResponseDTO;
+import com.upao.renteasegrupo1.backingservice.service.ContractService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contracts")
 public class ContractController {
+
+    private final ContractService contractService;
+
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContractResponseDTO>> getAllContracts() {
+        List<ContractResponseDTO> contracts = contractService.getAllContracts();
+        return ResponseEntity.ok(contracts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContractResponseDTO> getContractById(@PathVariable Long id) {
+        ContractResponseDTO contract = contractService.getContractById(id);
+        return ResponseEntity.ok(contract);
+    }// Se obtiene el id del contrato
+
+    @GetMapping("/duration")
+    public ResponseEntity<String> calculateDuration(@RequestParam Long Id) {
+        ContractResponseDTO contract = contractService.getContractById(Id);
+        int duration = contractService.calculateDuration(contract.getStartDate(), contract.getEndDate());
+        return ResponseEntity.ok("El contrato dura " + duration + " meses");
+    }
 }
--- src/main/java/com/upao/renteasegrupo1/backingservice/service/ContractService.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/service/ContractService.java
@@ -8,7 +8,10 @@ import com.upao.renteasegrupo1.backingservice.model.entity.Contract;
 import com.upao.renteasegrupo1.backingservice.repository.ContractRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Service
@@ -18,32 +21,51 @@ public class ContractService {
     private final ContractRepository contractRepository;
     private final ContractMapper contractMapper;
 
+    @Transactional(readOnly = true)
     public List<ContractResponseDTO> getAllContracts() {
         List<Contract> contracts = contractRepository.findAll();
         return contractMapper.convertToListDTO(contracts);
     }
 
+    @Transactional(readOnly = true)
     public ContractResponseDTO getContractById(Long id) {
         Contract contract = contractRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("Contrato no encontrado con el ID: " + id));
         return contractMapper.convertToDTO(contract);
     }
 
+    @Transactional
     public ContractResponseDTO createContract(ContractRequestDTO contractRequestDTO) {
         Contract contract = contractMapper.convertToEntity(contractRequestDTO);
-        contract.setStatus("Pendiente"); // Inicializar el estado del contrato a "Pendiente"
+        contract.setStatus("Pendiente"); // Estado inicial
         contract = contractRepository.save(contract);
         return contractMapper.convertToDTO(contract);
     }
 
-    public ContractResponseDTO updateContract(Long contractId, ContractRequestDTO contractRequestDTO) {
-        Contract existingContract = contractRepository.findById(contractId)
-            .orElseThrow(() -> new ResourceNotFoundException("Contrato no encontrado con el ID: " + contractId));
+    @Transactional
+    public ContractResponseDTO updateContract(Long Id, ContractRequestDTO contractRequestDTO) {
+        Contract contract = contractRepository.findById(Id)
+            .orElseThrow(() -> new ResourceNotFoundException("Contrato no encontrado con el ID: " + Id));
+        contract.setStartDate(contractRequestDTO.getStartDate());
+        contract.setEndDate(contractRequestDTO.getEndDate());
+        contractRepository.save(contract);
+        return contractMapper.convertToDTO(contract);
+    }
 
-        existingContract.setStartDate(contractRequestDTO.getStartDate());
-        existingContract.setEndDate(contractRequestDTO.getEndDate());
-        contractRepository.save(existingContract);
-        return contractMapper.convertToDTO(existingContract);
+    public int calculateDuration(LocalDate startDate, LocalDate endDate) {
+        if (startDate != null && endDate != null) {
+            Period period = Period.between(startDate, endDate);
+            return period.getYears() * 12 + period.getMonths();  // Convertir años a meses y añadir los meses adicionales
+        } else {
+            throw new IllegalArgumentException("Las fechas no pueden ser nulas");
+        }
+    }
+    
+
+    public void deleteContract(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteContract'");
     }
 }
+
 
