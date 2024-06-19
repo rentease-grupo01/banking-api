fd7c18bc967043650821b968c68bed9a8dc32243
SebasBazauri <86888189+SebasBazauri@users.noreply.github.com>
2024-06-19 17:04:38 -0500

    edit-Lodging update

--- src/main/java/com/upao/renteasegrupo1/backingservice/controller/LodgingController.java
+++ src/main/java/com/upao/renteasegrupo1/backingservice/controller/LodgingController.java
@@ -1,5 +1,6 @@
 package com.upao.renteasegrupo1.backingservice.controller;
 
+import com.upao.renteasegrupo1.backingservice.mapper.LodgingMapper;
 import com.upao.renteasegrupo1.backingservice.model.dto.LodgingRequestDTO;
 import com.upao.renteasegrupo1.backingservice.model.dto.LodgingResponseDTO;
 import com.upao.renteasegrupo1.backingservice.service.LodgingService;
@@ -15,11 +16,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/lodgings")
 public class LodgingController {
+    private final LodgingMapper lodgingMapper;
 
     private final LodgingService lodgingService;
 
-    public LodgingController(LodgingService lodgingService) {
+    public LodgingController(LodgingService lodgingService, LodgingMapper lodgingMapper) {
         this.lodgingService = lodgingService;
+        this.lodgingMapper = lodgingMapper;
     }
 
 
@@ -36,6 +39,13 @@ public class LodgingController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT); // Escenario Alternativo 1: No hay publicaciones activas
         }
         return new ResponseEntity<>(myLodgings, HttpStatus.OK); // Escenario Exitoso: Lista de publicaciones
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<LodgingResponseDTO> editLodging(
+            @PathVariable Long id,
+            @RequestBody LodgingRequestDTO lodgingRequestDTO) {
+        LodgingResponseDTO updatedLodging = lodgingService.editLodging(id, lodgingRequestDTO);
+        return new ResponseEntity<>(updatedLodging, HttpStatus.OK);
     }
 }
 
